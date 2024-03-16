b2cc8acc1407fd7750c09b8b7231c36956a3eb4e
Guangzong <chen@ggeta.com>
2024-03-15 23:02:36 -0400

    add usart

---
+++ src/bin/usart.rs
@@ -0,0 +1,40 @@
+#![feature(noop_waker)]
+#![no_std]
+#![no_main]
+use cortex_m_rt::entry;
+use u5_lib::clock;
+use u5_lib::gpio;
+use defmt_rtt as _;
+use gpio::GpioPort;
+use u5_lib::gpio::USART_RX_PA10;
+use u5_lib::gpio::USART_TX_PA9;
+use u5_lib::low_power::mcu_no_deep_sleep;
+use u5_lib::usart::USART1;
+const GREEN: GpioPort = gpio::PB7;
+const ORANGE: GpioPort = gpio::PC4;
+const BLUE: GpioPort = gpio::PC5;
+fn setup() {
+}
+
+#[entry]
+fn main() -> ! {
+    clock::init_clock();
+    mcu_no_deep_sleep();
+    setup();
+    USART1.setup(USART_TX_PA9, USART_RX_PA10);
+
+
+    defmt::info!("setup led finished!");
+    loop {
+        USART1.send("Hello, world!".as_bytes());
+        // GREEN.toggle();
+        // ORANGE.toggle();
+        clock::delay_ms(500);
+    }
+}
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
---
+++ src/usart.rs
@@ -0,0 +1,56 @@
+#![allow(unused)]
+
+use stm32_metapac::usart::vals::{M0, M1, Over8, Stop};
+
+use crate::{clock, gpio};
+
+pub struct UsartPort {
+    port: stm32_metapac::usart::Usart,
+}
+
+pub const USART1: UsartPort = UsartPort {
+    port: stm32_metapac::USART1
+};
+const USART_CLOCK: u32 = 16_000_000; // default use HSI16
+
+impl UsartPort {
+    /// current use default configuration, 115200 baudrate, 8 bit data, 1 stop bit, no parity
+    pub fn setup(&self, gpio_tx: gpio::GpioPort, gpio_rx: gpio::GpioPort) {
+        // todo!("enable usart1 clock and setup gpio for usart1");
+        gpio_tx.setup();
+        gpio_rx.setup();
+        clock::set_usart_clock();
+        // todo!("enable interrupt ");
+        // self.port
+        self.port.cr1().modify(|v| {
+            v.set_m0(M0::BIT8);
+            v.set_m1(M1::M0);
+            v.set_pce(false);
+            v.set_over8(Over8::OVERSAMPLING16); // oversampling by 16
+
+            v.set_ue(true);
+            v.set_te(true);
+            v.set_re(true);
+        });
+        self.port.cr2().modify(|v| {
+            v.set_stop(Stop::STOP1);   // 1 stop bit
+        });
+        self.port.cr3().modify(|v| {
+            // v.set_owr_ddr(true);
+        });
+        self.port.brr().write(|v| {
+            v.set_brr((USART_CLOCK / 115200) as u16);
+        });
+    }
+    pub fn send(&self, data: &[u8]) {
+        for &c in data {
+            while self.port.isr().read().txe() == false {}
+            self.port.tdr().write(|v| {
+                v.set_dr(c as u16);
+            });
+        }
+    }
+    pub fn recv(&self) -> u8 {
+        todo!();
+    }
+}
