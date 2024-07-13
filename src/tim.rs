078f916e6f274544a0ba9d97c558003a47a0ca50
Guangzong Chen <chen@ggeta.com>
2024-07-12 23:35:36 -0400

    update pwm for general timer

--- src/tim.rs
+++ src/tim.rs
@@ -7,18 +7,28 @@ use stm32_metapac::timer::vals::*;
 /// Should be careful the system clock may be changed depend on the system lode and the power mode.
 /// This clock will be affected by the system clock.
 use stm32_metapac::timer::TimAdv;
+use stm32_metapac::timer::TimGp16;
+use stm32_metapac::timer::TimGp32;
 use stm32_metapac::RCC;
 // todo!("The deepsleep mode does not working when this timer is using.");
 
-pub struct TimIns {
+pub struct TimAdvIns {
     ins: TimAdv,
     // init: bool,
 }
+pub struct TimBasicIns {
+    ins: TimGp32,
+}
 
-pub const TIM1: TimIns = TimIns {
+pub const TIM1: TimAdvIns = TimAdvIns {
     ins: stm32_metapac::TIM1,
     // init: false, // Some Timer setting are not allowed to change after the timer is enabled.
     //              // only capture and compare mode can be changed after the timer is enabled.
+};
+
+pub const TIM3: TimBasicIns = TimBasicIns {
+    ins: stm32_metapac::TIM3,
+    // init: false,
 };
 
 pub enum TimError {
@@ -60,7 +70,7 @@ impl Default for Config {
     }
 }
 
-impl TimIns {
+impl TimAdvIns {
     pub fn get_frequency() -> u32 {
         todo!("get the frequency of the timer.");
         // return 160_000_000 / (prescaler + 1) as u32;
@@ -74,7 +84,7 @@ impl TimIns {
                                                      // current suppose the main clock is 160MHz
     }
 
-    pub fn init(&mut self, config: Config) -> Result<(), TimError> {
+    pub fn init(&self, config: Config) -> Result<(), TimError> {
         // set_clock source
         // counter value is 0 -> arr (include 0 and arr).
         // So the tick is arr + 1;
@@ -129,5 +139,86 @@ impl TimIns {
             v.set_moe(true);
             v.set_aoe(true);
         });
+    }
+}
+
+impl TimBasicIns {
+    pub fn get_frequency() -> u32 {
+        todo!("get the frequency of the timer.");
+        // return 160_000_000 / (prescaler + 1) as u32;
+    }
+    pub fn set_clock(&self) {
+        // STM32 U5 TIM 1,8, 15, 16 and 17 use apb2 clock.
+        // It can be apb2 clock \times 1 or \times (not sure where to set it. But found this in the "clock tree" in the reference manual)
+        //
+        RCC.apb1enr1().modify(|v| v.set_tim3en(true)); // refer ot file header
+        self.ins.cr1().modify(|v| v.set_cen(false)); // disable counter for configuration
+                                                     // current suppose the main clock is 160MHz
+    }
+
+    pub fn init(&self, config: Config) -> Result<(), TimError> {
+        // set_clock source
+        // counter value is 0 -> arr (include 0 and arr).
+        // So the tick is arr + 1;
+        self.set_clock();
+        self.ins.cr1().modify(|v| {
+            v.set_dir(config.dir);
+            v.set_cms(config.cms);
+            v.set_arpe(config.arpe);
+        });
+        // self.ins.psc().write_value(0); // no prescaler
+        self.ins.psc().write(|_| config.prescaler);
+        // v.0 = config.prescaler);
+        // self.ins.rcr().modify(|v| v.set_rep(config.rcr));
+        self.ins.arr().write(|v| 160);
+        self.ins.cr1().modify(|v| v.set_cen(true)); // enable counter
+        self.ins.cr2().modify(|v| v.set_mms(config.mms));
+        Ok(())
+    }
+
+    pub fn set_pwm(&self, ch: u8, sum: u16, low: u16) {
+        // We use pwm mode 1 and the counter is upcounting.
+        // The output is high hwne `cnt < timccr1` and low when `cnt >= timccr1`
+        // Example: arr = 7, and timccr1 = 4; then we have high = 4 and low = 4;
+        // 160MHz --> 20MHz = 8
+        // arr = 160 and timccr = 80 then the output clock is 1MHz
+        let arr = sum;
+        // self.ins.arr().write(|v| v.0 = arr as u32);
+        // self.ins.arr().modify(|v| v = arr as &mut u32);
+        defmt::info!("arr: {}", arr);
+        self.ins.arr().write(|v| *v = arr as u32);
+        // self.ins.ccr(0).write(|v| v.0 = (duty_cycle * arr as f32) as u32);
+        // self.ins.ccr(0).write(|v| v.0 = low);
+        // self.ins.ccr((ch - 1) as _).write(|_| low as u32);
+        self.ins.ccr((ch - 1) as _).write(|v| *v = low as u32);
+        defmt::info!("arr: {}", self.ins.arr().read());
+        // ccr
+        defmt::info!("ccr: {}", self.ins.ccr(0).read());
+    }
+
+    pub fn enable_output(
+        &self,
+        // channel from 1 to 4
+        channel: u8,
+    ) {
+        let ch = channel - 1;
+        if ch <= 1 {
+            self.ins.ccmr_output(0).modify(|v| {
+                v.set_ccs(ch as _, CcmrOutputCcs::OUTPUT);
+                v.set_ocm(ch as _, Ocm::PWMMODE1);
+            });
+        } else {
+            // channel 3 and 4
+            self.ins.ccmr_output(1).modify(|v| {
+                v.set_ccs(ch as usize - 2, CcmrOutputCcs::OUTPUT);
+                v.set_ocm(ch as usize - 2, Ocm::PWMMODE1);
+            });
+        }
+        self.ins.ccer().modify(|v| v.set_cce(ch as _, true));
+        // self.ins.bdtr().modify(|v| {
+        //     v.set_moe(true);
+        //     v.set_aoe(true);
+        // });
+        self.ins.cr1().modify(|v| v.set_cen(true)); // enable counter
     }
 }
