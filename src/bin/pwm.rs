f2679fa49a61023579b4a2bae48e948f76003069
Guangzong Chen <chen@ggeta.com>
2024-07-15 10:45:53 -0400

    update tim example

--- src/bin/pwm.rs
+++ src/bin/pwm.rs
@@ -6,15 +6,16 @@
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 
-use tim::{Config, TIM1};
 use u5_lib::{
     clock::{self, delay_ms, delay_s, delay_us},
     com_interface::ComInterface,
     exti,
-    gpio::{self, GpioPort, TIM1_CH2_PA9, TIM1_CH3_PA10},
+    gpio::{self, GpioPort, TIM1_CH2_PA9, TIM1_CH3_PA10, TIM3_CH1_PA6},
     i2c::{self, I2c},
     low_power::{no_deep_sleep_request, Executor},
-    task, *,
+    task,
+    tim::{Config, TIM1, TIM3},
+    *,
 };
 const BLUE: GpioPort = gpio::PB7;
 
@@ -30,13 +31,19 @@ async fn async_main(spawner: Spawner) {
     );
     TIM1_CH2_PA9.setup();
     TIM1_CH3_PA10.setup();
-    TIM1.init(Config::default());
+    TIM3_CH1_PA6.setup();
+    let _ = TIM1.init(Config::default());
+    let _ = TIM3.init(Config::default());
     TIM1.set_pwm(1, 160, 80);
     TIM1.set_pwm(2, 160, 80);
     TIM1.set_pwm(3, 160, 80);
+    TIM3.set_pwm(1, 160, 80);
+    // TIM3.set_pwm(2, 160, 80);
     TIM1.enable_output(1);
     TIM1.enable_output(2);
     TIM1.enable_output(3);
+    TIM3.enable_output(1);
+    TIM3.enable_output(2);
     unsafe {
         no_deep_sleep_request();
     }
