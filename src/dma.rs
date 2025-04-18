32c704dff8ff5c84d233c84f1f8d2a58834426f3
Guangzong Chen <chen@ggeta.com>
2025-04-17 22:06:42 -0400

    update dma (test is required)

--- src/dma.rs
+++ src/dma.rs
@@ -1,8 +1,10 @@
 #![allow(unused)]
+
 use crate::clock::delay_tick;
 use crate::gpio::GpioPort;
-pub use stm32_metapac::gpdma::vals::*;
+use heapless::arc_pool;
 pub use stm32_metapac::gpdma::vals;
+pub use stm32_metapac::gpdma::vals::*;
 use stm32_metapac::gpdma::Channel;
 use stm32_metapac::gpdma::Gpdma;
 
@@ -18,7 +20,7 @@ pub struct DmaChannel {
     dst_inc: bool,
     src_type: Ap, //   // port0 for prepherial, port1 for memory
     dst_type: Ap,
-    tc_mode: vals::Tcem // ChTr2Tcem,
+    tc_mode: vals::Tcem, // ChTr2Tcem,
 }
 pub const DCMI_DMA: DmaChannel = DmaChannel {
     ins: stm32_metapac::GPDMA1,
@@ -74,78 +76,56 @@ impl DmaChannel {
             v.set_reqsel(self.request_source);
         });
     }
-    pub fn start(&self, src_addr: u32, dar_addr: u32, len: u32) {
+    pub async fn start(&self, src_addr: u32, dar_addr: u32, len: u32) {
         // buf: &[u8]) {
         self.init();
         assert!(len < DMA_MAX_TRANSFER_SIZE * 32);
-        // defmt::info!(
-        //     "dma src_addr = {}, dst_addr = {}, len = {}",
-        //     src_addr,
-        //     dar_addr,
-        //     len
-        // );
-        let cnts = (len + DMA_MAX_TRANSFER_SIZE - 1) / DMA_MAX_TRANSFER_SIZE; // round up
-
-        unsafe {
-            let link_list = &mut LINK_LISTS[self.ch];
-            // let link_list = &mut LINK_LISTS[self.ch];
-            for i in 0..(len / DMA_MAX_TRANSFER_SIZE) as usize {
-                if self.src_inc {
-                    link_list[i].sar = src_addr + (i as u32 * DMA_MAX_TRANSFER_SIZE);
-                } else {
-                    link_list[i].sar = src_addr;
+        let link_list = unsafe { &mut LINK_LISTS[self.ch] };
+        let num_link_list = (len + DMA_MAX_TRANSFER_SIZE - 1) / DMA_MAX_TRANSFER_SIZE;
+        for i in 0..num_link_list as usize {
+            link_list[i].sar = src_addr + (self.src_inc as u32) * (i as u32 * DMA_MAX_TRANSFER_SIZE);
+            link_list[i].dar = dar_addr + (self.dst_inc as u32) * (i as u32 * DMA_MAX_TRANSFER_SIZE);
+            link_list[i].llr = &link_list[i + 1] as *const _ as u32;
+            link_list[i].llr &= 0x0000ffff;
+            link_list[i].br1 = DMA_MAX_TRANSFER_SIZE;
+            // let addr = &mut link_list.val[1] as *mut _ as u32;
+            // set llr to update br(29), sar(28), dar(27), llr(16);
+            link_list[i].llr |= (1 << 29) | (1 << 28) | (1 << 27) | (1 << 16);
+            // handle last link list
+            if i == num_link_list as usize - 1 {
+                link_list[i].llr = 0;
+                if len % DMA_MAX_TRANSFER_SIZE != 0 {
+                    link_list[i].br1 = len % DMA_MAX_TRANSFER_SIZE;
                 }
-
-                if self.dst_inc {
-                    link_list[i].dar = dar_addr + (i as u32 * DMA_MAX_TRANSFER_SIZE);
-                } else {
-                    link_list[i].dar = dar_addr;
-                }
-                link_list[i].llr = &link_list[i + 1] as *const _ as u32;
-                link_list[i].llr &= 0x0000ffff;
-                link_list[i].br1 = DMA_MAX_TRANSFER_SIZE;
-                // let addr = &mut link_list.val[1] as *mut _ as u32;
-                // set llr to update br(29), sar(28), dar(27), llr(16);
-                link_list[i].llr |= (1 << 29) | (1 << 28) | (1 << 27) | (1 << 16);
             }
-
-            let mut last: usize = (len / DMA_MAX_TRANSFER_SIZE) as usize;
-            if (len % DMA_MAX_TRANSFER_SIZE) != 0 {
-                if self.src_inc {
-                    link_list[last].sar = src_addr + (last as u32 * DMA_MAX_TRANSFER_SIZE);
-                } else {
-                    link_list[last].sar = src_addr;
-                }
-                if self.dst_inc {
-                    link_list[last].dar = dar_addr + (last as u32 * DMA_MAX_TRANSFER_SIZE);
-                } else {
-                    link_list[last].dar = dar_addr;
-                }
-                link_list[last].br1 = len % DMA_MAX_TRANSFER_SIZE;
-                // link_list[last] = ListNode {
-                //     br1: 0,
-                //     sar: 0,
-                //     dar: 0,
-                //     llr: 0,
-                // };
-                link_list[last].llr = 0;
-            } else {
-                link_list[last - 1].llr = 0;
-            }
-            let ch = self.ins.ch(self.ch);
-            ch.lbar()
-                .write(|v| v.set_lba(((link_list.as_ptr() as u32) >> 16) as u16));
-            ch.sar().write_value(link_list[0].sar);
-            ch.dar().write_value(link_list[0].dar);
-            ch.br1().modify(|v| v.0 = link_list[0].br1);
-            ch.llr().modify(|v| v.0 = link_list[0].llr);
-            // defmt::info!("dma sar = 0x{:x}", ch.sar().read());
-            // defmt::info!("dma dar = 0x{:x}", ch.dar().read());
-
-            ch.cr().modify(|v| {
-                v.set_en(true);
-            });
         }
+        let ch = self.ins.ch(self.ch);
+        ch.lbar() .write(|v| v.set_lba(((link_list.as_ptr() as u32) >> 16) as u16));
+        ch.sar().write_value(link_list[0].sar);
+        ch.dar().write_value(link_list[0].dar);
+        ch.br1().modify(|v| v.0 = link_list[0].br1);
+        ch.llr().modify(|v| v.0 = link_list[0].llr);
+
+        ch.cr().modify(|v| {
+            v.set_en(true);
+        });
+
+        // enable interrupt
+        ch.cr().modify(|v| {
+            v.set_tcie(true);
+        });
+        // wait for finished interrupt and return
+        poll_fn(|cx| {
+            WAKER.register(cx.waker()); // register waker
+            // wait for the transfer complete
+            if ch.sr().read().tcf() {
+                ch.sr().write(|v| v.set_tcf(true));
+                // clear the interrupt
+                return core::task::Poll::Ready(());
+            } else {
+                return core::task::Poll::Pending;
+            }
+        });
     }
     pub fn stop(&self) {
         let ch = self.ins.ch(self.ch);
@@ -156,5 +136,27 @@ impl DmaChannel {
             v.set_reset(true);
         });
         while ch.cr().read().en() {}
+    }
+}
+use core::future::poll_fn;
+
+// waker
+use core::cell::Cell;
+use embassy_sync::waitqueue::AtomicWaker;
+static WAKER: AtomicWaker = AtomicWaker::new();
+use stm32_metapac::interrupt;
+/// DMA interrupt handler
+#[interrupt]
+fn GPDMA1_CHANNEL0() {
+    let ch = unsafe { stm32_metapac::GPDMA1.ch(0) };
+    if ch.sr().read().tcf() {
+        // ch.fcr().write(|v| v.set_tcf(true));
+        // clear the interrupt
+        // disable the interrupt
+        ch.cr().modify(|v| {
+            v.set_en(false);
+            v.set_tcie(false);
+        });
+        WAKER.wake();
     }
 }
