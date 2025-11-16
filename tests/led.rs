f3e954b3766d9f20edf3d6b0e4309c38ece7e8d6
Guangzong Chen <chen@ggeta.com>
2025-11-16 10:12:51 -0800

    update

--- tests/led.rs
+++ tests/led.rs
@@ -2,11 +2,9 @@
 #![no_main]
 
 use cortex_m_rt as _;
-use u5_lib as _;
-// force enable defmt in this file
 #[cfg(feature = "defmt")]
 use defmt_rtt as _;
-
+use u5_lib as _;
 #[embedded_test::tests]
 mod tests {
 
@@ -24,41 +22,59 @@ mod tests {
 
     #[test]
     fn test_led_160mhz() {
-        u5_lib::clock::init_clock(true, clock::ClockFreqs::KernelFreq160Mhz);
-        test_led();
+        test_led(clock::ClockFreqs::KernelFreq160Mhz);
     }
     #[test]
     fn test_led_80mhz() {
-        u5_lib::clock::init_clock(true, clock::ClockFreqs::KernelFreq80Mhz);
-        test_led();
+        test_led(clock::ClockFreqs::KernelFreq80Mhz);
     }
     #[test]
     fn test_led_40mhz() {
-        u5_lib::clock::init_clock(true, clock::ClockFreqs::KernelFreq40Mhz);
-        test_led();
+        test_led(clock::ClockFreqs::KernelFreq40Mhz);
+    }
+    #[test]
+    fn test_led_20mhz() {
+        test_led(clock::ClockFreqs::KernelFreq20Mhz);
+    }
+    #[test]
+    fn test_tim_160mhz() {
+        test_tim(clock::ClockFreqs::KernelFreq160Mhz);
+    }
+    #[test]
+    fn test_tim_80mhz() {
+        test_tim(clock::ClockFreqs::KernelFreq80Mhz);
+    }
+    #[test]
+    fn test_tim_40mhz() {
+        test_tim(clock::ClockFreqs::KernelFreq40Mhz);
+    }
+    #[test]
+    fn test_tim_20mhz() {
+        test_tim(clock::ClockFreqs::KernelFreq20Mhz);
     }
 
-    fn test_led() {
+    fn test_led(clock_freq: clock::ClockFreqs) {
+        clock::init_clock(true, clock_freq);
         u5_lib::necleo_u575::LED_BLUE.setup();
         u5_lib::necleo_u575::LED_GREEN.setup();
         u5_lib::necleo_u575::LED_RED.setup();
 
         u5_lib::necleo_u575::LED_BLUE.set_high();
-        delay_ms(500);
+        delay_ms(100);
         u5_lib::necleo_u575::LED_GREEN.set_high();
-        delay_ms(500);
+        delay_ms(100);
         u5_lib::necleo_u575::LED_RED.set_high();
-        delay_ms(500);
+        delay_ms(100);
         u5_lib::necleo_u575::LED_BLUE.set_low();
-        delay_ms(500);
+        delay_ms(100);
         u5_lib::necleo_u575::LED_GREEN.set_high();
-        delay_ms(500);
+        delay_ms(100);
         u5_lib::necleo_u575::LED_RED.set_low();
-        delay_ms(500);
+        delay_ms(100);
     }
 
-    #[test]
-    fn test_tim() {
+    fn test_tim(clock_freq: clock::ClockFreqs) {
+        clock::init_clock(true, clock_freq);
         let gpio = TIM3_CH1_PA6;
         gpio.setup();
         let tim = u5_lib::tim::TIM3;
